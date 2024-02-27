c432997a33a590e9f1910b3234b4e7bf347b9ddb
Grazi 1988 <grazi_510@hotmail.com>
2024-02-27 18:53:41 -0300

    alteracao

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,16 @@
 import React from "react"
 import "./Footer.css"
 
-function Footer(){
-    return(
-        <footer>
-            <h2> Eu sou o footer </h2>
-
-
-        </footer>
-    )
+function Footer () {
+  return (
+    <div className="footer-container">
+      <div className="contact-info">
+        <p>whatsapp:(11)9 8311-8635</p>
+        <p>Envie uma mensagem: atendimento@petshop.com</p>
+        <p>Horário de funcionamento: Segunda a Sexta das 08:00 ás 17:00hrs</p>
+      </div>
+    </div>
+  )
 }
 
 export default Footer
